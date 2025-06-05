2d7203ad517fd18b1e5f45876ff4e51b7a20a301
HasChad <troll.yahya@gmail.com>
2025-06-06 01:44:01 +0300

    - trying to add bullets

--- src/main.zig
+++ src/main.zig
@@ -1,15 +1,16 @@
 const rl = @import("raylib");
 const std = @import("std");
 
-const Gravity = 1;
+const Gravity = 0.5;
 const Player = struct {
     pos: rl.Vector2,
     size: rl.Vector2,
 };
 const Bullet = struct {
     pos: rl.Vector2,
+    vel_y: f32,
     index: i32,
-    vel_y: i32,
+    ttl: f32,
     // is_active: bool,
 };
 const Ball = struct {
@@ -23,6 +24,7 @@ const Ball = struct {
 pub fn main() anyerror!void {
     const screenWidth = 500;
     const screenHeight = 700;
+    const allocator = std.heap.page_allocator;
 
     rl.initWindow(screenWidth, screenHeight, "Zig-Balls");
     rl.setTargetFPS(60);
@@ -46,26 +48,38 @@ pub fn main() anyerror!void {
         .size = @as(f32, @floatFromInt(100)) / 2.0,
     };
 
-    const single_bullet = Bullet{
-        .pos = rl.Vector2{ .x = 100, .y = 100 },
-        .index = 1,
-        .vel_y = 100,
-    };
-
-    const bullets = [5]Bullet{
-        single_bullet,
-        single_bullet,
-        single_bullet,
-        single_bullet,
-        single_bullet,
-    };
+    var bullets_hash = std.AutoHashMap(u32, Bullet).init(allocator);
+    defer bullets_hash.deinit();
 
     // Game loop
     while (!rl.windowShouldClose()) {
         // ! Update
         // std.debug.print("mouse x pos = {}\n", .{rl.getMouseX()});
-
         rl.setWindowTitle(rl.textFormat("Zig-Balls - FPS: %i", .{rl.getFPS()}));
+
+        if (rl.isMouseButtonDown(.left)) {
+            const size: u32 = bullets_hash.count();
+
+            std.debug.print("size = {}\n", .{size});
+
+            try bullets_hash.put(size, Bullet{
+                .pos = player.pos,
+                .index = 1,
+                .vel_y = 0.2,
+                .ttl = 3.0,
+            });
+        }
+
+        var bullets = bullets_hash.iterator();
+
+        while (bullets.next()) |bullet| {
+            bullet.value_ptr.ttl -= rl.getFrameTime();
+            if (bullet.value_ptr.ttl < 0.0) {
+                try bullets_hash.remove(bullet.key_ptr.*);
+            } else {
+                bullet.value_ptr.pos.y += bullet.value_ptr.vel_y;
+            }
+        }
 
         player.pos.x = @as(f32, @floatFromInt(rl.getMouseX())) - @divExact(player.size.x, 2);
 
@@ -116,11 +130,10 @@ pub fn main() anyerror!void {
         //rl.drawTextEx(rl.font, rl.textFormat("%i", .{ball.current_point}), ball.pos, 20, 0, rl.Color.white);
 
         // bullets
-        for (bullets) |bullet| {
-            rl.drawLineEx(
-                bullet.pos,
-                rl.Vector2{ .x = bullet.pos.x, .y = bullet.pos.y - 20.0 },
-                2,
+        while (bullets.next()) |bullet| {
+            rl.drawCircleV(
+                bullet.value_ptr.pos,
+                3,
                 rl.Color.yellow,
             );
         }
