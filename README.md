38d82a447ebf43c634909ae09324e36c263bfd87
HasChad <troll.yahya@gmail.com>
2025-12-18 01:20:54 +0300

    - updated build.zig
    - added readme.md

---
+++ README.md
@@ -0,0 +1,6 @@
+# Zig-Balls
+
+Shooting jumping ball game clone made with Zig and Raylib.
+
+Mouse to Move  
+Left Click: Shoot
--- build.zig
+++ build.zig
@@ -3,11 +3,10 @@ const std = @import("std");
 pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
-
-    // const mod = b.addModule("zig_balls", .{
-    //     .root_source_file = b.path("src/root.zig"),
-    //     .target = target,
-    // });
+    const raylib_dep = b.dependency("raylib_zig", .{
+        .target = target,
+        .optimize = optimize,
+    });
 
     const exe = b.addExecutable(.{
         .name = "zig_balls",
@@ -16,23 +15,14 @@ pub fn build(b: *std.Build) void {
             .target = target,
             .optimize = optimize,
             .imports = &.{
-                // .{ .name = "zig_balls", .module = mod },
+                .{ .name = "raylib", .module = raylib_dep.module("raylib") }, // main raylib module
+                .{ .name = "raygui", .module = raylib_dep.module("raygui") }, // raygui module
             },
         }),
     });
 
-    const raylib_dep = b.dependency("raylib_zig", .{
-        .target = target,
-        .optimize = optimize,
-    });
-
-    const raylib = raylib_dep.module("raylib"); // main raylib module
-    const raygui = raylib_dep.module("raygui"); // raygui module
     const raylib_artifact = raylib_dep.artifact("raylib"); // raylib C library
-
     exe.linkLibrary(raylib_artifact);
-    exe.root_module.addImport("raylib", raylib);
-    exe.root_module.addImport("raygui", raygui);
 
     b.installArtifact(exe);
 
@@ -44,19 +34,4 @@ pub fn build(b: *std.Build) void {
     if (b.args) |args| {
         run_cmd.addArgs(args);
     }
-
-    // const mod_tests = b.addTest(.{
-    //     .root_module = mod,
-    // });
-    // const run_mod_tests = b.addRunArtifact(mod_tests);
-
-    const exe_tests = b.addTest(.{
-        .root_module = exe.root_module,
-    });
-
-    const run_exe_tests = b.addRunArtifact(exe_tests);
-
-    const test_step = b.step("test", "Run tests");
-    // test_step.dependOn(&run_mod_tests.step);
-    test_step.dependOn(&run_exe_tests.step);
 }
